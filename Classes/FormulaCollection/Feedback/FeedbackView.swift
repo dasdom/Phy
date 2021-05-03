bf9784d7a7381801fd4f6b157aa01da930f51e95
Dominik Hauser <dom@dasdom.de>
2021-05-03 11:21:59 +0200

    Add feedback view; add some solvers

---
+++ Classes/FormulaCollection/Feedback/FeedbackView.swift
@@ -0,0 +1,19 @@
+//  Created by Dominik Hauser on 03/05/2021.
+//  Copyright © 2021 dasdom. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeedbackView: View {
+  var body: some View {
+    VStack {
+      Text("Hast du Verbesserungsvorschläge? Fehlt dir eine Formel?")
+    }
+  }
+}
+
+struct FeedbackView_Previews: PreviewProvider {
+  static var previews: some View {
+    FeedbackView()
+  }
+}
---
+++ Classes/General/AppStrings.swift
@@ -0,0 +1,5 @@
+//  Created by Dominik Hauser on 03/05/2021.
+//  Copyright © 2021 dasdom. All rights reserved.
+//
+
+import Foundation
