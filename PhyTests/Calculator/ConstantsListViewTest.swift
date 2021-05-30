dd50f2ab374704b963e7d770aad009444f8a97e9
Dominik Hauser <dom@dasdom.de>
2021-05-30 19:45:37 +0200

    Take snapshots

--- PhyTests/Calculator/ConstantsListViewTest.swift
+++ PhyTests/Calculator/ConstantsListViewTest.swift
@@ -18,7 +18,8 @@ class ConstantsListViewTest: XCTestCase {
   
   func test_constantsListView_snapshotTest() {
     let view = ConstantsListView()
-    assertSnapshot(matching: view, as: .image)
+    
+    assertSnapshot(matching: view, as: .image, record: true)
   }
   
 }
